15e021d944e5ecc3d1ed02d76037ed766480b44a
Septen <gammerxpower@gmail.com>
2021-02-16 03:22:44 +0200

    EthCustodian improvements. Add ProofKeeper. Fixes.

--- eth-custodian/contracts/EthCustodian.sol
+++ eth-custodian/contracts/EthCustodian.sol
@@ -1,87 +1,63 @@
 pragma solidity ^0.6;
-import "rainbow-bridge/contracts/eth/nearprover/contracts/INearProver.sol";
+
 import "rainbow-bridge/contracts/eth/nearprover/contracts/ProofDecoder.sol";
 import "rainbow-bridge/contracts/eth/nearbridge/contracts/Borsh.sol";
 
-contract EthCustodian {
-    using Borsh for Borsh.Data;
-    using ProofDecoder for Borsh.Data;
+import { INearProver, ProofKeeper } from "./ProofKeeper.sol";
 
-    INearProver public prover_;
-    bytes public nearEvm_;
-
-    // OutcomeReciptId -> Used
-    mapping(bytes32 => bool) public usedEvents_;
-
-    event Locked (
+contract EthCustodian is ProofKeeper {
+    event Deposited (
         address indexed sender,
+        address indexed ethRecipientOnNear,
         uint256 amount,
-        uint256 fee,
-        address ethRecipientOnNear,
+        uint256 fee
     );
 
-    event Unlocked (
-        uint128 amount,
-        address recipient
+    event Withdrawn(
+        address indexed recipient,
+        uint128 amount
     );
 
-    // Function output from burning fungible token on Near side.
+    // Function output from burning nETH on Near side.
     struct BurnResult {
         uint128 amount;
         address recipient;
     }
 
-    // EthLocker is linked to the bridge token factory on NEAR side.
-    // It also links to the prover that it uses to unlock the tokens.
-    constructor(bytes memory nearEvm, INearProver prover, address admin) public {
-        require(nearEvm.length > 0, "Invalid Near EVM address");
-        require(address(prover) != address(0), "Invalid Near prover");
-
-        nearEvm_ = nearEvm;
-        prover_ = prover;
+    /// EthCustodian is linked to the EVM on NEAR side.
+    /// It also links to the prover that it uses to withdraw the tokens.
+    constructor(bytes memory nearEvm, INearProver prover, address admin)
+        ProofKeeper(nearEvm, prover)
+        public
+    {
         admin_ = admin;
     }
 
     /// Deposits the specified amount of provided ETH (except from the relayer's fee) into the smart contract.
     /// `ethRecipientOnNear` - the ETH address of recipient in NEAR EVM
     /// `fee` - the amount of fee that will be paid to the near-relayer in nETH.
-    function deposit(address ethRecipientOnNear, uint256 fee) payable external {
+    function deposit(address ethRecipientOnNear, uint256 fee)
+        external
+        payable
+    {
         require(fee < msg.value, "The fee cannot be bigger than the transferred amount.");
-        emit Locked(msg.sender, msg.value - fee, fee, ethRecipientOnNear);
+        emit Deposited(msg.sender, ethRecipientOnNear, msg.value, fee);
     }
 
     /// Withdraws the appropriate amount of ETH which is encoded in `proofData`
-    function withdraw(bytes memory proofData, uint64 proofBlockHeight) external {
+    function withdraw(bytes calldata proofData, uint64 proofBlockHeight) external
+    {
         ProofDecoder.ExecutionStatus memory status = _parseAndConsumeProof(proofData, proofBlockHeight);
         BurnResult memory result = _decodeBurnResult(status.successValue);
-        address(this).transfer(result.recipient, result.amount);
-        emit Unlocked(result.amount, result.recipient);
+        payable(result.recipient).transfer(result.amount);
+        emit Withdrawn(result.recipient, result.amount);
     }
 
-    function _parseAndConsumeProof(bytes memory proofData, uint64 proofBlockHeight)
+    function _decodeBurnResult(bytes memory data)
         internal
-        returns (ProofDecoder.ExecutionStatus memory result)
+        pure
+        returns (BurnResult memory result)
     {
-        require(prover_.proveOutcome(proofData, proofBlockHeight), "Proof should be valid");
-
-        // Unpack the proof and extract the execution outcome.
-        Borsh.Data memory borshData = Borsh.from(proofData);
-        ProofDecoder.FullOutcomeProof memory fullOutcomeProof = borshData.decodeFullOutcomeProof();
-        require(borshData.finished(), "Argument should be exact borsh serialization");
-
-        bytes32 receiptId = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.receipt_ids[0];
-        require(!usedEvents_[receiptId], "The burn event cannot be reused");
-        usedEvents_[receiptId] = true;
-
-        require(keccak256(fullOutcomeProof.outcome_proof.outcome_with_id.outcome.executor_id) == keccak256(nearTokenFactory_),
-                "Can only unlock tokens from the linked mintable fungible token on Near blockchain.");
-
-        result = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.status;
-        require(!result.failed, "Cannot use failed execution outcome for unlocking the tokens.");
-        require(!result.unknown, "Cannot use unknown execution outcome for unlocking the tokens.");
-    }
-
-    function _decodeBurnResult(bytes memory data) internal pure returns (BurnResult memory result) {
         Borsh.Data memory borshData = Borsh.from(data);
         result.amount = borshData.decodeU128();
         bytes20 recipient = borshData.decodeBytes20();
@@ -95,11 +71,18 @@ contract EthCustodian {
         _;
     }
 
-    function adminTransfer(IERC20 token, address destination, uint amount) public onlyAdmin {
-        token.safeTransfer(destination, amount);
+    function adminTransfer(address payable destination, uint amount)
+        public
+        onlyAdmin
+    {
+        destination.transfer(amount);
     }
 
-    function adminDelegatecall(address target, bytes memory data) public onlyAdmin returns(bytes memory) {
+    function adminDelegatecall(address target, bytes memory data)
+        public
+        onlyAdmin
+        returns (bytes memory)
+    {
         (bool success, bytes memory rdata) = target.delegatecall(data);
         require(success);
         return rdata;
