2c2f54b5ebc249577a905936617eb13c2cc788a0
Septen <gammerxpower@gmail.com>
2021-02-20 01:22:07 +0200

    Add more tests and proof template. Improvements.

--- eth-custodian/test/EthCustodian.js
+++ eth-custodian/test/EthCustodian.js
@@ -1,6 +1,3 @@
-const { BN, constants } = require('@openzeppelin/test-helpers');
-const { ZERO_ADDRESS } = constants;
-
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
@@ -9,8 +6,14 @@ const { borshifyOutcomeProof } = require('rainbow-bridge-lib/rainbow/borshify-pr
 
 //const NearProverMock = artifacts.require('test/NearProverMock')
 
-//const { toWei, fromWei, hexToBytes } = web3.utils;
-
+const SCHEMA = {
+  'Withdrawn': {
+    kind: 'struct', fields: [
+      ['recipient', [20]],
+      ['amount', 'u128'],
+    ]
+  }
+};
 
 describe('EthCustodian contract', () => {
     let nearProverMockContractFactory;
@@ -20,12 +23,13 @@ describe('EthCustodian contract', () => {
     let deployerAccount;
     let adminAccount;
     let walletUser1;
+    let user2;
     let ethRecipientOnNear;
 
     const nearEvmAccount = Buffer.from('evm.near');
 
     beforeEach(async () => {
-        [deployerAccount, ethRecipientOnNear] = await ethers.getSigners();
+        [deployerAccount, ethRecipientOnNear, user2] = await ethers.getSigners();
 
         // Make the deployer admin
         adminAccount = deployerAccount;
@@ -45,7 +49,7 @@ describe('EthCustodian contract', () => {
         // TODO move this test to ProofKeeper tests
         it('Should revert when prover is zero address', async () => {
             await expect(
-                ethCustodianContractFactory.deploy(nearEvmAccount, ZERO_ADDRESS, adminAccount.address)
+                ethCustodianContractFactory.deploy(nearEvmAccount, ethers.constants.AddressZero, adminAccount.address)
             )
                 .to
                 .be
@@ -80,7 +84,8 @@ describe('EthCustodian contract', () => {
                 .deposit(ethRecipientOnNear.address, fee);
 
             unsigned_tx.nonce = await ethers.provider.getTransactionCount(walletUser1.address);
-            unsigned_tx.value = 100;
+            // Set the value equal to fee. This should fail as the fee should be less than the transferred amount
+            unsigned_tx.value = fee;
 
             let signed_tx = await walletUser1.signTransaction(unsigned_tx);
             await expect(
@@ -101,7 +106,7 @@ describe('EthCustodian contract', () => {
             unsigned_tx.nonce = await ethers.provider.getTransactionCount(walletUser1.address);
             unsigned_tx.value = 50000;
 
-            let balanceBefore = await ethers.provider.getBalance(ethCustodian.address);
+            let balanceBefore = ethers.BigNumber.from(await ethers.provider.getBalance(ethCustodian.address));
 
             let signed_tx = await walletUser1.signTransaction(unsigned_tx);
             await expect(
@@ -111,13 +116,98 @@ describe('EthCustodian contract', () => {
                 .emit(ethCustodian, 'Deposited')
                 .withArgs(walletUser1.address, ethRecipientOnNear.address, unsigned_tx.value, 100);
 
-            let balanceAfter = await ethers.provider.getBalance(ethCustodian.address);
+            let balanceAfter = ethers.BigNumber.from(await ethers.provider.getBalance(ethCustodian.address));
+            let balanceDiff = balanceAfter.sub(balanceBefore);
             await expect(
-                balanceAfter
+                balanceDiff
             )
                 .to
-                .equal(balanceBefore + unsigned_tx.value);
+                .equal(unsigned_tx.value);
         });
     });
 
+    describe('Withdraw', () => {
+        const proof = require('./proof_template.json');
+        const proof_executor_id = proof.outcome_proof.outcome.executor_id;
+
+        beforeEach(async () => {
+            let fee = 100;  // wei
+            let unsigned_tx = await ethCustodian
+                .connect(walletUser1)
+                .populateTransaction
+                .deposit(ethRecipientOnNear.address, fee);
+
+            unsigned_tx.nonce = await ethers.provider.getTransactionCount(walletUser1.address);
+            unsigned_tx.value = 123000;
+
+            let signed_tx = await walletUser1.signTransaction(unsigned_tx);
+            await ethers.provider.sendTransaction(signed_tx)
+        });
+
+        it('Should revert when the proof producer (nearEvmAccount) is differs from the linked one', async () => {
+            let amount = 5000;
+            proof.outcome_proof.outcome.status.SuccessValue = serialize(SCHEMA, 'Withdrawn', {
+                recipient: ethers.utils.arrayify(user2.address),
+                amount: amount,
+            }).toString('base64');
+            // Manually set the incorrect proof producer
+            proof.outcome_proof.outcome.executor_id = 'evm2.near';
+
+            await expect(
+                ethCustodian.withdraw(borshifyOutcomeProof(proof), 1099)
+            )
+                .to
+                .be
+                .revertedWith('Can only unlock tokens from the linked proof producer on Near blockchain');
+        });
+
+        it('Should successfully withdraw and emit the withdrawn event', async () => {
+            let amount = 5000;
+            proof.outcome_proof.outcome.status.SuccessValue = serialize(SCHEMA, 'Withdrawn', {
+                recipient: ethers.utils.arrayify(user2.address),
+                amount: amount,
+            }).toString('base64');
+            // Set the executor_id manually (orginal proof executor) due to cloning issues
+            proof.outcome_proof.outcome.executor_id = proof_executor_id;
+
+            let balanceBefore = ethers.BigNumber.from(await ethers.provider.getBalance(user2.address));
+
+            await expect(
+                ethCustodian.withdraw(borshifyOutcomeProof(proof), 1099)
+            )
+                .to
+                .emit(ethCustodian, 'Withdrawn')
+                .withArgs(user2.address, amount);
+
+            let balanceAfter = ethers.BigNumber.from(await ethers.provider.getBalance(user2.address));
+            let balanceDiff = balanceAfter.sub(balanceBefore);
+
+            await expect(
+                balanceDiff
+            )
+                .to
+                .equal(amount)
+        });
+
+        it('Should revert when trying to use the same proof twice', async () => {
+            let amount = 5000;
+            proof.outcome_proof.outcome.status.SuccessValue = serialize(SCHEMA, 'Withdrawn', {
+                recipient: ethers.utils.arrayify(user2.address),
+                amount: amount,
+            }).toString('base64');
+            // Set the executor_id manually (orginal proof executor) due to cloning issues
+            proof.outcome_proof.outcome.executor_id = proof_executor_id;
+
+            // Withdraw for the first time
+            await ethCustodian.withdraw(borshifyOutcomeProof(proof), 1099);
+
+            // Try to withdraw for the second time providing the same proof
+            await expect(
+                ethCustodian.withdraw(borshifyOutcomeProof(proof), 1099)
+            )
+                .to
+                .be
+                .revertedWith('The burn event cannot be reused');
+        });
+    });
 });
