2c8ac266527b644580062e537c90d030d3e830a1
Evgeny Ukhanov <mrlsd@ya.ru>
2021-03-18 16:13:40 +0200

    Added EthContract separeted privat implementation

--- pure-eth-connector/src/connector.rs
+++ pure-eth-connector/src/connector.rs
@@ -1,17 +1,17 @@
 use super::*;
 
-struct EthConnectorContract {
+pub struct EthConnectorContract {
     contract: EthConnector,
 }
 
 impl EthConnectorContract {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             contract: sdk::get_contract_data(),
         }
     }
 
-    fn init_contract() {
+    pub fn init_contract() {
         //assert!(!sdk::state_exists(), "Contract already initialized");
         #[cfg(feature = "log")]
         sdk::log("[init contract]".into());
@@ -29,7 +29,7 @@ impl EthConnectorContract {
         sdk::save_contract(&contract_data);
     }
 
-    fn deposit(&self) {
+    pub fn deposit(&self) {
         sdk::assert_one_yocto();
         #[cfg(feature = "log")]
         sdk::log("[Deposit tokens]".into());
@@ -91,115 +91,92 @@ impl EthConnectorContract {
         );
         sdk::promise_return(promise1);
     }
-}
 
-#[no_mangle]
-pub extern "C" fn new() {
-    EthConnectorContract::init_contract()
-}
+    pub fn finish_deposit(&mut self) {
+        sdk::assert_private_call();
+        let data: FinishDepositCallArgs =
+            FinishDepositCallArgs::try_from_slice(&sdk::read_input()).unwrap();
+        #[cfg(feature = "log")]
+        sdk::log(format!("Finish deposit amount: {:?}", data.amount));
+        assert_eq!(sdk::promise_results_count(), 1);
+        let data0: Vec<u8> = match sdk::promise_result(0) {
+            PromiseResult::Successful(x) => x,
+            _ => panic!("Promise with index 0 failed"),
+        };
+        #[cfg(feature = "log")]
+        sdk::log("Check verification_success".into());
+        let verification_success: bool = bool::try_from_slice(&data0).unwrap();
+        assert!(verification_success, "Failed to verify the proof");
+        self.record_proof(data.proof.get_key());
 
-#[no_mangle]
-pub extern "C" fn deposit() {
-    EthConnectorContract::new().deposit()
-}
-
-#[no_mangle]
-pub extern "C" fn finish_deposit() {
-    sdk::assert_private_call();
-    let data: FinishDepositCallArgs =
-        FinishDepositCallArgs::try_from_slice(&sdk::read_input()).unwrap();
-    #[cfg(feature = "log")]
-    sdk::log(format!("Finish deposit amount: {:?}", data.amount));
-    assert_eq!(sdk::promise_results_count(), 1);
-    let data0: Vec<u8> = match sdk::promise_result(0) {
-        PromiseResult::Successful(x) => x,
-        _ => panic!("Promise with index 0 failed"),
-    };
-    #[cfg(feature = "log")]
-    sdk::log("Check verification_success".into());
-    let verification_success: bool = bool::try_from_slice(&data0).unwrap();
-    assert!(verification_success, "Failed to verify the proof");
-    //record_proof(data.proof.get_key());
-
-    // Mint tokens to recipient minus fee
-    mint(data.new_owner_id, data.amount - data.fee);
-    // Mint fee for Predecessor
-    mint(sdk::predecessor_account_id(), data.fee);
-}
-
-fn record_proof(key: Vec<u8>) -> Balance {
-    let mut contract: EthConnector = sdk::get_contract_data();
-    let initial_storage = sdk::storage_usage();
-
-    assert!(
-        !contract.used_events.contains(&key[..]),
-        "Proof event cannot be reused. Proof already exist."
-    );
-    contract.used_events.insert(key);
-    let current_storage = sdk::storage_usage();
-    let attached_deposit = sdk::attached_deposit();
-    let required_deposit =
-        Balance::from(current_storage - initial_storage) * sdk::STORAGE_PRICE_PER_BYTE;
-    sdk::save_contract(&contract);
-    attached_deposit - required_deposit
-}
-
-#[no_mangle]
-pub extern "C" fn verify_log_entry() {
-    #[cfg(feature = "log")]
-    sdk::log("Call from verify_log_entry".into());
-    let data = true.try_to_vec().unwrap();
-    sdk::value_return(&data[..]);
-}
-
-fn mint(owner_id: AccountId, amount: Balance) {
-    #[cfg(feature = "log")]
-    sdk::log(format!("Mint {:?} tokens for: {:?}", amount, owner_id));
-    let mut contract: EthConnector = sdk::get_contract_data();
-
-    let owner_id_key: &str = owner_id.as_ref();
-    if contract.token.accounts.get(owner_id_key).is_none() {
-        // TODO: NEP-145 Account Storage implementation fee
-        // It spent additional account amount for storage
-        contract.token.accounts.insert(owner_id.clone(), 0);
+        // Mint tokens to recipient minus fee
+        self.mint(data.new_owner_id, data.amount - data.fee);
+        // Mint fee for Predecessor
+        self.mint(sdk::predecessor_account_id(), data.fee);
     }
-    contract.token.internal_deposit(&owner_id, amount);
-    sdk::save_contract(&contract);
-    #[cfg(feature = "log")]
-    sdk::log("Mint success".into());
-}
 
-#[no_mangle]
-pub extern "C" fn ft_total_supply() {
-    let contract: EthConnector = sdk::get_contract_data();
-    let total_supply = contract.token.ft_total_supply();
-    sdk::value_return(&total_supply.to_be_bytes());
-    #[cfg(feature = "log")]
-    sdk::log(format!("Total supply: {}", total_supply));
-}
+    fn record_proof(&mut self, key: Vec<u8>) -> Balance {
+        let initial_storage = sdk::storage_usage();
 
-#[no_mangle]
-pub extern "C" fn ft_balance_of() {
-    let args: BalanceOfCallArgs = serde_json::from_slice(&sdk::read_input()[..]).unwrap();
-    let contract: EthConnector = sdk::get_contract_data();
-    let balance = contract.token.ft_balance_of(args.account_id.clone());
-    sdk::value_return(&balance.to_be_bytes());
-    #[cfg(feature = "log")]
-    sdk::log(format!("Balance [{}]: {}", args.account_id, balance));
-}
+        assert!(
+            !self.contract.used_events.contains(&key[..]),
+            "Proof event cannot be reused. Proof already exist."
+        );
+        self.contract.used_events.insert(key);
+        let current_storage = sdk::storage_usage();
+        let attached_deposit = sdk::attached_deposit();
+        let required_deposit =
+            Balance::from(current_storage - initial_storage) * sdk::STORAGE_PRICE_PER_BYTE;
+        self.save_contract();
+        attached_deposit - required_deposit
+    }
 
-#[no_mangle]
-pub extern "C" fn ft_transfer() {
-    let args: TransferCallArgs = serde_json::from_slice(&sdk::read_input()[..]).unwrap();
+    fn mint(&mut self, owner_id: AccountId, amount: Balance) {
+        #[cfg(feature = "log")]
+        sdk::log(format!("Mint {:?} tokens for: {:?}", amount, owner_id));
 
-    let mut contract: EthConnector = sdk::get_contract_data();
-    contract
-        .token
-        .ft_transfer(args.receiver_id.clone(), args.amount, args.memo.clone());
-    sdk::save_contract(&contract);
-    #[cfg(feature = "log")]
-    sdk::log(format!(
-        "Transfer to {} amount {} with memo {:?} success",
-        args.receiver_id, args.amount, args.memo
-    ));
+        let owner_id_key: &str = owner_id.as_ref();
+        if self.contract.token.accounts.get(owner_id_key).is_none() {
+            // TODO: NEP-145 Account Storage implementation fee
+            // It spent additional account amount for storage
+            self.contract.token.accounts.insert(owner_id.clone(), 0);
+        }
+        self.contract.token.internal_deposit(&owner_id, amount);
+        self.save_contract();
+        #[cfg(feature = "log")]
+        sdk::log("Mint success".into());
+    }
+
+    pub fn ft_total_supply(&self) {
+        let total_supply = self.contract.token.ft_total_supply();
+        sdk::value_return(&total_supply.to_be_bytes());
+        #[cfg(feature = "log")]
+        sdk::log(format!("Total supply: {}", total_supply));
+    }
+
+    pub fn ft_balance_of(&self) {
+        let args: BalanceOfCallArgs = serde_json::from_slice(&sdk::read_input()[..]).unwrap();
+        let balance = self.contract.token.ft_balance_of(args.account_id.clone());
+        sdk::value_return(&balance.to_be_bytes());
+        #[cfg(feature = "log")]
+        sdk::log(format!("Balance [{}]: {}", args.account_id, balance));
+    }
+
+    pub fn ft_transfer(&mut self) {
+        let args: TransferCallArgs = serde_json::from_slice(&sdk::read_input()[..]).unwrap();
+
+        self.contract
+            .token
+            .ft_transfer(args.receiver_id.clone(), args.amount, args.memo.clone());
+        self.save_contract();
+        #[cfg(feature = "log")]
+        sdk::log(format!(
+            "Transfer to {} amount {} with memo {:?} success",
+            args.receiver_id, args.amount, args.memo
+        ));
+    }
+
+    fn save_contract(&mut self) {
+        sdk::save_contract(&self.contract);
+    }
 }
--- pure-eth-connector/src/lib.rs
+++ pure-eth-connector/src/lib.rs
@@ -12,6 +12,7 @@ mod prover;
 mod sdk;
 mod types;
 
+pub use crate::connector::EthConnectorContract;
 pub use crate::deposit_event::*;
 pub use crate::fungible_token::*;
 pub use crate::log_entry::*;
@@ -50,4 +51,43 @@ pub unsafe fn on_panic(info: &core::panic::PanicInfo) -> ! {
 #[no_mangle]
 pub unsafe fn on_alloc_error(_: core::alloc::Layout) -> ! {
     core::intrinsics::abort();
+}
+
+#[no_mangle]
+pub extern "C" fn new() {
+    EthConnectorContract::init_contract()
+}
+
+#[no_mangle]
+pub extern "C" fn deposit() {
+    EthConnectorContract::new().deposit()
+}
+
+#[no_mangle]
+pub extern "C" fn finish_deposit() {
+    EthConnectorContract::new().finish_deposit();
+}
+
+#[no_mangle]
+pub extern "C" fn ft_total_supply() {
+    EthConnectorContract::new().ft_total_supply();
+}
+
+#[no_mangle]
+pub extern "C" fn ft_balance_of() {
+    EthConnectorContract::new().ft_balance_of();
+}
+
+#[no_mangle]
+pub extern "C" fn ft_transfer() {
+    EthConnectorContract::new().ft_transfer();
+}
+
+/// TODO: will be removed - for eth-prover tests only
+#[no_mangle]
+pub extern "C" fn verify_log_entry() {
+    #[cfg(feature = "log")]
+    sdk::log("Call from verify_log_entry".into());
+    let data = true.try_to_vec().unwrap();
+    sdk::value_return(&data[..]);
 }
