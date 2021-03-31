4ae78d5264257af2d3b259635596570198f539b1
Evgeny Ukhanov <mrlsd@ya.ru>
2021-03-31 13:44:48 +0300

    Integration tests for Deposit

--- tests-pure-eth-connector/tests/connector.rs
+++ tests-pure-eth-connector/tests/connector.rs
@@ -1,8 +1,22 @@
 #![allow(unused_variables)]
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{near_bindgen, AccountId};
 
 #[near_bindgen]
 pub struct EthConnector;
+
+#[derive(Default, BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Proof {
+    pub log_index: u64,
+    pub log_entry_data: Vec<u8>,
+    pub receipt_index: u64,
+    pub receipt_data: Vec<u8>,
+    pub header_data: Vec<u8>,
+    pub proof: Vec<Vec<u8>>,
+    pub skip_bridge_call: bool,
+}
 
 #[near_bindgen]
 impl EthConnector {
@@ -10,4 +24,17 @@ impl EthConnector {
     pub fn new(prover_account: AccountId, eth_custodian_address: String) {}
 
     pub fn ft_balance_of(&self, account_id: AccountId) {}
+
+    #[payable]
+    pub fn deposit(
+        &mut self,
+        log_index: u64,
+        log_entry_data: Vec<u8>,
+        receipt_index: u64,
+        receipt_data: Vec<u8>,
+        header_data: Vec<u8>,
+        proof: Vec<Vec<u8>>,
+        skip_bridge_call: bool,
+    ) {
+    }
 }
