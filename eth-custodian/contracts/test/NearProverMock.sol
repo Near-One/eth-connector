841ba15843c8e5b74ee4b5f27acadf875d2a20cb
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-05-12 15:22:44 +0200

    add license identifier

--- eth-custodian/contracts/test/NearProverMock.sol
+++ eth-custodian/contracts/test/NearProverMock.sol
@@ -1,15 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.6.12;
 
 import "rainbow-bridge/contracts/eth/nearprover/contracts/INearProver.sol";
 
 contract NearProverMock is INearProver {
-    function proveOutcome(bytes memory proofData, uint64 blockHeight) override public view returns(bool) {
+    function proveOutcome(
+        bytes memory proofData, 
+        uint64 blockHeight
+    )
+        public
+        view
+        override 
+    returns(bool) {
         return true;
     }
 }
 
 contract NearNegativeProverMock is INearProver {
-    function proveOutcome(bytes memory proofData, uint64 blockHeight) override public view returns(bool) {
+    function proveOutcome(
+        bytes memory proofData, 
+        uint64 blockHeight
+    ) 
+        public
+        view
+        override 
+    returns(bool) {
         return false;
     }
 }
