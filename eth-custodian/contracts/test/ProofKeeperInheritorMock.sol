10c8212de86fbd8b363539ebc199ed0b9802005a
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-05-12 15:22:44 +0200

    add license identifier

--- eth-custodian/contracts/test/ProofKeeperInheritorMock.sol
+++ eth-custodian/contracts/test/ProofKeeperInheritorMock.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.6.12;
 
 import "rainbow-bridge/contracts/eth/nearprover/contracts/ProofDecoder.sol";
@@ -5,13 +6,20 @@ import "rainbow-bridge/contracts/eth/nearprover/contracts/ProofDecoder.sol";
 import { INearProver, ProofKeeper } from "../ProofKeeper.sol";
 
 contract ProofKeeperInheritorMock is ProofKeeper {
-    constructor(bytes memory nearProofProducerAccount, INearProver prover, uint64 minBlockAcceptanceHeight)
+    constructor(
+        bytes memory nearProofProducerAccount, 
+        INearProver prover, 
+        uint64 minBlockAcceptanceHeight
+    )
         ProofKeeper(nearProofProducerAccount, prover, minBlockAcceptanceHeight)
         public
     {
     }
 
-    function parseAndConsumeProof(bytes memory proofData, uint64 blockHeight)
+    function parseAndConsumeProof(
+        bytes memory proofData, 
+        uint64 blockHeight
+    )
         public
     {
         _parseAndConsumeProof(proofData, blockHeight);
