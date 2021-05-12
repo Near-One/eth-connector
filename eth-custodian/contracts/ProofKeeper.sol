23e67956b2fab0bad821645e5772943a831b3abb
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-05-12 15:08:35 +0200

    minor style fixes

--- eth-custodian/contracts/ProofKeeper.sol
+++ eth-custodian/contracts/ProofKeeper.sol
@@ -18,9 +18,21 @@ contract ProofKeeper {
     // OutcomeReciptId -> Used
     mapping(bytes32 => bool) public usedEvents_;
 
-    constructor(bytes memory nearProofProducerAccount, INearProver prover, uint64 minBlockAcceptanceHeight) public {
-        require(nearProofProducerAccount.length > 0, "Invalid Near ProofProducer address");
-        require(address(prover) != address(0), "Invalid Near prover address");
+    constructor(
+        bytes memory nearProofProducerAccount,
+        INearProver prover,
+        uint64 minBlockAcceptanceHeight
+    ) 
+        public 
+    {
+        require(
+            nearProofProducerAccount.length > 0,
+            "Invalid Near ProofProducer address"
+        );
+        require(
+            address(prover) != address(0),
+            "Invalid Near prover address"
+        );
 
         nearProofProducerAccount_ = nearProofProducerAccount;
         prover_ = prover;
@@ -29,28 +41,51 @@ contract ProofKeeper {
 
     /// Parses the provided proof and consumes it if it's not already used.
     /// The consumed event cannot be reused for future calls.
-    function _parseAndConsumeProof(bytes memory proofData, uint64 proofBlockHeight)
+    function _parseAndConsumeProof(
+        bytes memory proofData, 
+        uint64 proofBlockHeight
+    )
         internal
-        returns (ProofDecoder.ExecutionStatus memory result)
+        returns(ProofDecoder.ExecutionStatus memory result)
     {
-        require(proofBlockHeight >= minBlockAcceptanceHeight_, "Proof is from the ancient block");
-        require(prover_.proveOutcome(proofData, proofBlockHeight), "Proof should be valid");
+        require(
+            proofBlockHeight >= minBlockAcceptanceHeight_,
+            "Proof is from the ancient block"
+        );
+        require(
+            prover_.proveOutcome(proofData,proofBlockHeight),
+            "Proof should be valid"
+        );
 
         // Unpack the proof and extract the execution outcome.
         Borsh.Data memory borshData = Borsh.from(proofData);
         ProofDecoder.FullOutcomeProof memory fullOutcomeProof = borshData.decodeFullOutcomeProof();
-        require(borshData.finished(), "Argument should be exact borsh serialization");
+        require(
+            borshData.finished(),
+            "Argument should be exact borsh serialization"
+        );
 
         bytes32 receiptId = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.receipt_ids[0];
-        require(!usedEvents_[receiptId], "The burn event cannot be reused");
+        require(
+            !usedEvents_[receiptId],
+            "The burn event cannot be reused"
+        );
         usedEvents_[receiptId] = true;
 
-        require(keccak256(fullOutcomeProof.outcome_proof.outcome_with_id.outcome.executor_id)
-                == keccak256(nearProofProducerAccount_),
-                "Can only withdraw coins from the linked proof producer on Near blockchain");
+        require(
+            keccak256(fullOutcomeProof.outcome_proof.outcome_with_id.outcome.executor_id)
+            == keccak256(nearProofProducerAccount_),
+            "Can only withdraw coins from the linked proof producer on Near blockchain"
+        );
 
         result = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.status;
-        require(!result.failed, "Cannot use failed execution outcome for unlocking the tokens");
-        require(!result.unknown, "Cannot use unknown execution outcome for unlocking the tokens");
+        require(
+            !result.failed, 
+            "Cannot use failed execution outcome for unlocking the tokens"
+        );
+        require(
+            !result.unknown,
+            "Cannot use unknown execution outcome for unlocking the tokens"
+        );
     }
 }
