0b3719487f0f7adda44e2391e73cf1a2b88a266d
Septen <septengineering@pm.me>
2022-01-23 21:48:54 +0000

    Remove redundant & obsolete files.

--- evm-fungible-token/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release || exit 1
-mkdir -p res
-cp target/wasm32-unknown-unknown/release/eth_connector.wasm ./res/
-
-# wasm-opt -Oz --output ./res/near_evm.wasm ./res/near_evm.wasm
-ls -lh res/eth_connector.wasm
-# rm -rf target
