e45e721fb695ce1409df48a2e693a824ee21c7d7
Septen <kirill@aurora.dev>
2022-01-29 01:12:14 +0000

    Fix broken import

--- eth-custodian/contracts/test/NearProverMock.sol
+++ eth-custodian/contracts/test/NearProverMock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8;
 
-import 'rainbow-bridge/contracts/eth/nearprover/contracts/INearProver.sol';
+import 'rainbow-bridge-sol/nearprover/contracts/INearProver.sol';
 
 contract NearProverMock is INearProver {
     function proveOutcome(
